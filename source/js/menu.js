08dce432f765f195b25d3468c77ea578195f568d
allexfree <allexfree@yandex.ru>
2021-09-13 17:08:10 +0300

    Interactive for menu, continue page-header

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,4 +1,5 @@
 const topMenuSecond = {
+  self: document.querySelector('.topmenu-second'),
   list: document.querySelector('.menu-list-js'),
   items: document.querySelectorAll('.menu-item-js'),
   links: document.querySelectorAll('.menu-link-js')
@@ -8,14 +9,13 @@ console.log(topMenuSecond.items);
 
 const menuClickHandler = (evt)=> {
   let target = evt.target;
-  topMenuSecond.links.forEach((link)=> {
-    link.onfocus = function () {
-      console.log('yes');
-    }
-    link.onblur = function () {
-      console.log('no');
-    }
-  })
+  console.log(target);
+  if (target !== topMenuSecond.list) {
+    topMenuSecond.links.forEach((link)=> {
+      link.classList.remove('topmenu-second__list-link--active');
+      target.classList.add('topmenu-second__list-link--active');
+    })
+  }
 };
 
 export default topMenuSecond.list.addEventListener('click', menuClickHandler);
